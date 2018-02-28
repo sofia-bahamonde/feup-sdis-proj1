38e9ceb65ae71614d98220b788c07d0bba1c0fc7
kyahra <up201504570@B03AV0812.fe.up.pt>
2018-02-28 13:00:08 +0000

    lab02 completed

--- L02/src/Client.java
+++ L02/src/Client.java
@@ -2,92 +2,83 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 
 
 public class Client {
 	
-  public static void main(String[] args) throws IOException{
-	  
-    if(!valid_arguments(args)) return;
-    
-	int port = Integer.parseInt(args[1]);
-	InetAddress address = InetAddress.getByName(args[0]);
-	DatagramSocket socket = new DatagramSocket();
+	public static int mcast_port;
+	public static int srvc_port;
+	private static InetAddress mcast_addr;
+	private static InetAddress srvc_addr;
 
-	byte[] s_buffer = new byte[1024];
-	byte[] r_buffer = new byte[1024];
-	
-	// send request
-	System.out.println("Sending Request");
-	String request = create_request(args);
-	s_buffer = request.getBytes();
-	DatagramPacket sendPacket = new DatagramPacket(s_buffer, s_buffer.length, address, port);
-	socket.send(sendPacket);
-	
-	// receive response
-	DatagramPacket receivePacket = new DatagramPacket(r_buffer, r_buffer.length);
-	socket.receive(receivePacket);
-	String response = new String(receivePacket.getData());
-	
-	// print response
-	System.out.println("RESPONSE:" + response);
-	
-	// end connection
-	socket.close();
-	System.out.println("Connection End");
-	
-	return;
+	public static void main(String[] args) throws IOException {
 
+		if (args.length > 5 || args.length < 4) {
+			System.out.println("ERROR - Invalid arguments");
+			System.out.println("Usage - Client <mcast_addr> <mcast_port> <oper> <opnd>*");
+			return;
+		}
 
-  }
-  
-  private static String create_request(String[] args) {
-	  String request= "";
-	  
-	  request += args[2].toUpperCase() + " ";
-	  
-	  for(int i =3; i <args.length; i++)
-		  request += args[i] + " ";
-		  
-	  request = request.substring(0, request.length() - 1);
-	  
-	  return request;
-  }
+		mcast_addr = InetAddress.getByName(args[0]);
+		mcast_port = Integer.parseInt(args[1]);
+		
+		// join multicast group 
+		MulticastSocket mcast_socket = new MulticastSocket(mcast_port);
+		mcast_socket.joinGroup(mcast_addr);
 
-  private static boolean valid_arguments(String[] args){
-	  
-	  if(args[2].equals("register")){
-		  if(args.length != 5) {
-		  System.out.println("ERROR - Invalid number of operands");
-		  return false;
-		  }
-	  }else if(args[2].equals("lookup")) {
-		  if(args.length != 4) {
-		  System.out.println("ERROR - Invalid number of operands");
-		  return false;
-	  	}
-	  }else {
-		  System.out.println("ERROR - Invalid operation");
-		  return false;
-	  }
-	  
-	  if(!valid_plate(args[3])) {
-		  System.out.println("ERROR - Invalid plate");
-		  return false;
-	  }
-	  
-	
-	  return true;
+		// learn address
+		byte[] buf = new byte[1024];
+		DatagramPacket mcast_packet = new DatagramPacket(buf, buf.length);
+		mcast_socket.receive(mcast_packet);
+		String msg = new String(buf, 0, buf.length).trim();
+		
+		
+		// get address
+		String[] array = msg.split("\\s");
+		srvc_addr = InetAddress.getByName(array[0]);
+		srvc_port = Integer.parseInt(array[1]);
+		System.out.println("multicast: " + mcast_addr + " " + mcast_port + " : " +srvc_addr + " " + srvc_port);
+		
+		// initialize datagram socket
+		DatagramSocket socket = new DatagramSocket();
 
-  }
+		byte[] s_buffer = new byte[1024];
+		byte[] r_buffer = new byte[1024];
 
-private static boolean valid_plate(String plate) {
-	if(plate.length() != 8) return false;
-	
-	if(plate.charAt(2) != '-' || plate.charAt(5) != '-' )return false;
-	
-	return true;
-}
+		// send request
+		System.out.println("Sending Request");
+		String request = create_request(args);
+		s_buffer = request.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(s_buffer, s_buffer.length, srvc_addr, srvc_port);
+		socket.send(sendPacket);
 
+		// receive response
+		DatagramPacket receivePacket = new DatagramPacket(r_buffer, r_buffer.length);
+		socket.receive(receivePacket);
+		String response = new String(receivePacket.getData());
+
+		// print response
+		System.out.println(request + " :: " + response);
+
+		// end connection
+		socket.close();
+		System.out.println("Connection End");
+		return;
+
+	}
+
+	private static String create_request(String[] args) {
+		String request = "";
+
+		request += args[2].toUpperCase() + " ";
+
+		for (int i = 3; i < args.length; i++)
+			request += args[i] + " ";
+
+		request = request.substring(0, request.length() - 1);
+
+		return request;
+	}
 
 }
--- L02/src/Server.java
+++ L02/src/Server.java
@@ -2,87 +2,130 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.util.Hashtable;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
-public class Server {
+public class Server extends TimerTask{
 	
   private static Hashtable<String, String> data_base;
-
-  public static void main(String[] args) throws IOException{
-	  
-	  if(args.length!=1) {
-		  System.out.println("ERROR - Invalid arguments");
-		  System.out.println("Usage - Server <port_number>");
-		  return;
-	  }
-	  
-	  data_base = new Hashtable<String, String>();
-	  
-	  int r_port = Integer.parseInt(args[0]);
-	  DatagramSocket socket = new DatagramSocket(r_port);
-	  
-	  
-      while (true) {
-    	  
-    	byte[] r_buffer = new byte[1024];
-        byte[] s_buffer = new byte[1024];
-    	  
+  public static int srvc_port;
+  public static int mcast_port;
+  private static InetAddress mcast_addr;
+  private static String srvc_addr= "localhost";
+  static Timer timer;
+  static MulticastSocket mcast_socket;
   
-    	// receive request 
-		DatagramPacket receivePacket = new DatagramPacket(r_buffer, r_buffer.length);
-		socket.receive(receivePacket);
-		String request = new String(receivePacket.getData());
-		System.out.println("RECEIVED: " + request);
-		
-		// handle request
-		InetAddress adress = receivePacket.getAddress();
-		int s_port = receivePacket.getPort();
-		String response = handleRequest(request);
-		
-		// send response
-		s_buffer = response.getBytes();
-		DatagramPacket sendPacket = new DatagramPacket(s_buffer, s_buffer.length, adress, s_port);
-		socket.send(sendPacket);
+  
+	@Override
+	public void run() {
+		// create multicast datagram packet
+		String msg = srvc_addr + " " + srvc_port;
+		DatagramPacket mcast_packet = new DatagramPacket(msg.getBytes(), msg.getBytes().length, mcast_addr, mcast_port);
+
+		try {
+			mcast_socket.send(mcast_packet);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-      
-      /*
-      System.out.println("Ending Connection");
-      socket.close();
-      System.out.println("Server Terminated");
-      */
-
-  }
-
-private static String handleRequest(String request) {
-	String[] array = request.split(" ");
-	
-	String request_type = array[0];
-	String plate = array[1];
-	
-	if(request_type.equals("REGISTER")) {
-		if(!data_base.containsKey(plate)) {
-			String name = "";
-			
-			for(int i =2; i < array.length; i++) 
-				name += array[i] + " ";
-			
-			name = name.substring(0, name.length() - 1);
-			
-			data_base.put(plate, name);
-			return Integer.toString(data_base.size());
-			
-		}else return "-1";
-			
-	}else if(request_type.equals("LOOKUP")) {
 		
-		if(data_base.containsKey(plate)) {
-			return data_base.get(plate);
-		}else return "NOT_FOUND";
+		System.out.println(
+				"multicast: " + mcast_addr + " " + mcast_port + " : " + srvc_addr + " " + srvc_port);
+
 	}
+
+
+
+	public static void main(String[] args) throws IOException {
+
+		if (args.length != 3) {
+			System.out.println("ERROR - Invalid arguments");
+			System.out.println("Usage - Server <srvc_port> <mcast_addr> <mcast_port>");
+			return;
+		}
+
+		srvc_port = Integer.parseInt(args[0]);
+		mcast_addr = InetAddress.getByName(args[1]);
+		mcast_port = Integer.parseInt(args[2]);
+
+		data_base = new Hashtable<String, String>();
+
+		DatagramSocket socket = new DatagramSocket(srvc_port);
+		
+		// create MulticastSocket
+		mcast_socket = new MulticastSocket(mcast_port);
+		mcast_socket.setTimeToLive(1);
+		
+		TimerTask timerTask = new Server();
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(timerTask, 0, 1*1000);
+
+		while (true) {
+
+			byte[] r_buffer = new byte[1024];
+			byte[] s_buffer = new byte[1024];
+
+			// receive request
+			DatagramPacket receivePacket = new DatagramPacket(r_buffer, r_buffer.length);
+			socket.receive(receivePacket);
+			String request = new String(receivePacket.getData());
+
+			// handle request
+			InetAddress s_address = receivePacket.getAddress();
+			int s_port = receivePacket.getPort();
+			String response = handleRequest(request);
+			
+			System.out.println(request + " :: " + response);
+			
 	
-	return "-1";
-}
+			// send response
+			s_buffer = response.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(s_buffer, s_buffer.length, s_address, s_port);
+			socket.send(sendPacket);
+		}
+
+		/*
+		 * System.out.println("Ending Connection"); socket.close();
+		 * System.out.println("Server Terminated");
+		 */
+
+	}
+
+	private static String handleRequest(String request) {
+		String[] array = request.split(" ");
+
+		String request_type = array[0];
+		String plate = array[1];
+
+		if (request_type.equals("REGISTER")) {
+			if (!data_base.containsKey(plate)) {
+				String name = "";
+
+				for (int i = 2; i < array.length; i++)
+					name += array[i] + " ";
+
+				name = name.substring(0, name.length() - 1);
+
+				data_base.put(plate, name);
+				return Integer.toString(data_base.size());
+
+			} else
+				return "-1";
+
+		} else if (request_type.equals("LOOKUP")) {
+
+			if (data_base.containsKey(plate)) {
+				return data_base.get(plate);
+			} else
+				return "NOT_FOUND";
+		}
+
+		return "-1";
+	}
+
 
 
 }
