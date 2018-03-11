65551cca33f83d369b5f7e144bfe28a51e1cb5c0
kyahra <sofiacbahamonde@gmail.com>
2018-03-11 17:26:38 +0000

    simple RMI connection

---
+++ Project/Peer.java
@@ -0,0 +1,32 @@
+import common.Hello;
+
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+        
+public class Peer implements Hello {
+        
+    public Peer() {}
+
+    public String sayHello() {
+        return "Hello, world!";
+    }
+        
+    public static void main(String args[]) {
+        
+        try {
+            Peer obj = new Peer();
+            Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("Hello", stub);
+
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
